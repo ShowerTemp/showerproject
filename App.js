6a39e2fbae28ad93e3abd3f266020bc845d999b0
mero <arsmohamed@outlook.com>
2021-07-18 02:51:36 +0200

    working on the main page

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.MainScreen}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+        <TextInput 
+          placeholder="my application goals" 
+          style={{borderColor : 'black',borderWidth: 1}}/>
+        <Button title="Lets see" />
+      </View>
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  MainScreen: {
+    padding: 30, 
+    marginTop: 40,
+    marginBottom: 30,
+    margin: 5,
+    borderRadius: 15,
+    height: '100%',
+    backgroundColor: 'blue'
+  }
 });
